637f04c842bec1a6a1f6c65b27c8b633b5d6bcbe
Romimap <Romimap75@gmail.com>
2021-10-05 13:38:28 +0200

    Co-authored-by: FeuFeve <FeuFeve@users.noreply.github.com>

--- Main.cs
+++ Main.cs
@@ -8,14 +8,12 @@ public class Main : Spatial
     // private string b = "text";
 
     // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        
+    public override void _Ready() {
+        GD.Print("Hello World !");
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta) {
+        
+    }
 }
