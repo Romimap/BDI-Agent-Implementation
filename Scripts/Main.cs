b90214a51f067555884bc40fd172319a5059a790
FeuFeve <clement-potin@outlook.com>
2021-10-18 15:41:12 +0200

    Fix MapCreation.cs and move/rename objects

--- Scripts/Main.cs
+++ Scripts/Main.cs
@@ -4,29 +4,29 @@ using System.Collections.Generic;
 
 public class Main : Spatial
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+	// Declare member variables here. Examples:
+	// private int a = 2;
+	// private string b = "text";
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready() {
-        GD.Print("Hello World !");
-        WorldState realWorld = new WorldState("./Maps/map001.png");
-        GD.Print(realWorld);
-        System.Console.WriteLine(realWorld);
-        foreach(KeyValuePair<string, Agent> kvp in realWorld.Agents) {
-            GD.Print(kvp.Value.PrintBeliefs());    
-        }
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready() {
+		GD.Print("Hello World !");
+		WorldState realWorld = new WorldState("./Maps/map001.png");
+		GD.Print(realWorld);
+		System.Console.WriteLine(realWorld);
+		foreach(KeyValuePair<string, Agent> kvp in realWorld.Agents) {
+			GD.Print(kvp.Value.PrintBeliefs());    
+		}
 
-        realWorld.Tick();
-        foreach(KeyValuePair<string, Agent> kvp in realWorld.Agents) {
-            GD.Print(kvp.Value.PrintBeliefs());    
-        }
+		realWorld.Tick();
+		foreach(KeyValuePair<string, Agent> kvp in realWorld.Agents) {
+			GD.Print(kvp.Value.PrintBeliefs());    
+		}
 
-    }
+	}
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(float delta) {
-        
-    }
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(float delta) {
+		
+	}
 }
--- Scripts/MapCreation.cs
+++ Scripts/MapCreation.cs
@@ -2,112 +2,92 @@ using Godot;
 using System;
 using System.Drawing;
 
-public class Main : Spatial
+public class MapCreation : Spatial
 {
-	Spatial visibleMap;
-	Spatial invisibleMap;
+    Spatial visibleMap;
+    Spatial invisibleMap;
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		visibleMap = (Spatial)GetNode("VisibleMap");
-		invisibleMap = (Spatial)GetNode("InvisibleMap");
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        visibleMap = (Spatial)GetNode("VisibleMap");
+        invisibleMap = (Spatial)GetNode("InvisibleMap");
 
-		// Load prefabs
-		PackedScene wallObject = (PackedScene)ResourceLoader.Load("res://Objects/Wall 2.tscn");
-		PackedScene wallObjectTransparent = (PackedScene)ResourceLoader.Load("res://Objects/Wall (not seen) 2.tscn");
-		PackedScene groundTileObject = (PackedScene)ResourceLoader.Load("res://Objects/GroundTile 2.tscn");
-		PackedScene groundTileObjectTransparent = (PackedScene)ResourceLoader.Load("res://Objects/GroundTile (not seen) 2.tscn");
+        // Load prefabs
+        PackedScene wallObject = (PackedScene)ResourceLoader.Load("res://Entities/Wall.tscn");
+        PackedScene wallObjectTransparent = (PackedScene)ResourceLoader.Load("res://Entities/Wall (ghost).tscn");
+        PackedScene groundTileObject = (PackedScene)ResourceLoader.Load("res://Entities/GroundTile.tscn");
+        PackedScene groundTileObjectTransparent = (PackedScene)ResourceLoader.Load("res://Entities/GroundTile (ghost).tscn");
 
-		// Load bitmap
-		Bitmap bitmap = new Bitmap("./Bitmaps/Map 4.png");
+        // Load bitmap
+        Bitmap bitmap = new Bitmap("./Bitmaps/Map 4.png");
 
-		// Create terrain
-		for (int i = 0; i < bitmap.Width; i++)
-		{
-			for (int j = 0; j < bitmap.Height; j++)
-			{
-				Spatial instance = null;
+        // Create terrain
+        for (int i = 0; i < bitmap.Width; i++)
+        {
+            for (int j = 0; j < bitmap.Height; j++)
+            {
+                Spatial instance = null;
 
-				System.Drawing.Color pixel = bitmap.GetPixel(i, j);
-				string pixelHexValue = "#" + pixel.R.ToString("X2") + pixel.G.ToString("X2") + pixel.B.ToString("X2");
+                System.Drawing.Color pixel = bitmap.GetPixel(i, j);
+                string pixelHexValue = "#" + pixel.R.ToString("X2") + pixel.G.ToString("X2") + pixel.B.ToString("X2");
 
-				bool isVisible = false;
-				switch (pixelHexValue)
-				{
-					case "#000000": // Wall
-						if (i < bitmap.Width / 2.0f && j < bitmap.Height / 2.0f)
-						{
-							instance = (Spatial)wallObject.Instance();
-							isVisible = true;
-						}
-						else
-						{
-							instance = (Spatial)wallObjectTransparent.Instance();
-						}
-						break;
+                bool isVisible = false;
+                switch (pixelHexValue)
+                {
+                    case "#000000": // Wall
+                        if (i < bitmap.Width / 2.0f && j < bitmap.Height / 2.0f)
+                        {
+                            instance = (Spatial)wallObject.Instance();
+                            isVisible = true;
+                        }
+                        else
+                        {
+                            instance = (Spatial)wallObjectTransparent.Instance();
+                        }
+                        break;
 
-					case "#FFFFFF": // Ground tile
-						if (i < bitmap.Width / 2.0f && j < bitmap.Height / 2.0f)
-						{
-							instance = (Spatial)groundTileObject.Instance();
-							isVisible = true;
-						}
-						else
-						{
-							instance = (Spatial)groundTileObjectTransparent.Instance();
-						}
-						break;
+                    case "#FFFFFF": // Ground tile
+                        if (i < bitmap.Width / 2.0f && j < bitmap.Height / 2.0f)
+                        {
+                            instance = (Spatial)groundTileObject.Instance();
+                            isVisible = true;
+                        }
+                        else
+                        {
+                            instance = (Spatial)groundTileObjectTransparent.Instance();
+                        }
+                        break;
 
-					default:
-						break;
-				}
+                    default:
+                        break;
+                }
 
-				instance.Translate(new Vector3(i, 0, j));
-				if (isVisible)
-					visibleMap.AddChild(instance);
-				else
-					invisibleMap.AddChild(instance);
-			}
-			GD.Print(i);
-		}
+                instance.Translate(new Vector3(i, 0, j));
+                if (isVisible)
+                    visibleMap.AddChild(instance);
+                else
+                    invisibleMap.AddChild(instance);
+            }
+            GD.Print(i);
+        }
 
-		// Move camera
-		Camera camera = (Camera)GetNode("./Camera");
-		Transform transform = camera.Transform;
+        // Move camera
+        Camera camera = (Camera)GetNode("./Camera");
+        Transform transform = camera.Transform;
 
-		// CENTERED CAMERA
-		// transform.origin = new Vector3(bitmap.Width / 2.0f, (bitmap.Width + bitmap.Height) / 2.0f, bitmap.Height / 2.0f);
-		// PERSPECTIVE CAMERA
-		// transform.origin = new Vector3(bitmap.Width, (bitmap.Width + bitmap.Height) / 3.0f, bitmap.Height);
-		// transform = transform.LookingAt(new Vector3(2 * bitmap.Width / 3.0f, 0, 2 * bitmap.Height / 3.0f), new Vector3(0, 1, 0));
-		// ORTHOGONAL CAMERA
-		int w = bitmap.Width;
-		int h = bitmap.Height;
-		int maxWH = Math.Max(w, h);
+        // ORTHOGONAL CAMERA
+        int w = bitmap.Width;
+        int h = bitmap.Height;
+        int maxWH = Math.Max(w, h);
 
-		transform.origin = new Vector3(w / 2.0f, maxWH, h / 2.0f);
-		transform.origin += new Vector3(maxWH, maxWH, maxWH);
-		transform = transform.LookingAt(new Vector3(w / 2.0f, 0, h / 2.0f), new Vector3(0, 1, 0));
-		// transform.origin += new Vector3(w / 2.0f, maxWH, h / 2.0f);
-		camera.Size = 3 + maxWH * 1.2f;
+        transform.origin = new Vector3(w / 2.0f, maxWH, h / 2.0f);
+        transform.origin += new Vector3(maxWH, maxWH, maxWH);
+        transform = transform.LookingAt(new Vector3(w / 2.0f, 0, h / 2.0f), new Vector3(0, 1, 0));
+        camera.Size = 3 + maxWH * 1.2f;
 
-		// transform.origin = new Vector3(1.5f * maxWH, maxWH, 1.5f * maxWH);
-		// transform = transform.LookingAt(new Vector3(maxWH / 2.0f, 0, maxWH / 2.0f), new Vector3(0, 1, 0));
-		// camera.Size = 5 + maxWH;
+        camera.Transform = transform;
 
-		// transform.origin = new Vector3(bitmap.Width, (bitmap.Width + bitmap.Height) / 3.0f, bitmap.Height);
-		// transform = transform.LookingAt(new Vector3(bitmap.Width / 2.0f, 0, bitmap.Height / 2.0f), new Vector3(0, 1, 0));
-		// camera.Size = 5 + (bitmap.Width + bitmap.Height) / 2.0f;
-
-		camera.Transform = transform;
-
-		GD.Print("Done");
-	}
-
-	//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-	//  public override void _Process(float delta)
-	//  {
-	//      
-	//  }
+        GD.Print("Done");
+    }
 }
